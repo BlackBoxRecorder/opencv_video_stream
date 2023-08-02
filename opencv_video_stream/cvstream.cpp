4fa690415bfe8c970aa7ac3cc26d3599bc91573c
yinnan <timetickme@outlook.com>
2023-08-02 21:06:58 +0800

    add img http api

--- opencv_video_stream/cvstream.cpp
+++ opencv_video_stream/cvstream.cpp
@@ -5,125 +5,148 @@ using namespace cv;
 
 cvstream::cvstream(std::string source, std::string dist, int width, int height, int quality)
 {
-  this->videosource = source;
-  this->stream_url = dist;
-  this->height = height;
-  this->width = width;
-  this->quality = quality;
+    this->videosource = source;
+    this->stream_url = dist;
+    this->height = height;
+    this->width = width;
+    this->quality = quality;
 }
 
 void cvstream::run()
 {
-  rtspThread = std::thread(&cvstream::read_source, this);
+    rtspThread = std::thread(&cvstream::read_source, this);
 
-  if (rtspThread.joinable())
-  {
-    rtspThread.join();
-  }
+    svr.Get("/img", [this](const httplib::Request&, httplib::Response& res)
+        {
+            if (imgString.length() > 0)
+            {
+                res.set_content(imgString, "image/jpeg");
+            }
+            else
+            {
+                res.set_content("none", "text/plain");
+            }
+        });
+
+    httpThread = std::thread([&]()
+        {
+            cout << "start http server" << endl;
+            svr.listen("127.0.0.1", 9999);
+        });
+
+    if (rtspThread.joinable())
+    {
+        rtspThread.join();
+    }
 }
 
 cvstream::~cvstream()
 {
-  isWorking = false;
+    isWorking = false;
 
-  if (rtspThread.joinable())
-  {
-    rtspThread.join();
-  }
+    if (rtspThread.joinable())
+    {
+        rtspThread.join();
+    }
 
-  if (streamThread.joinable())
-  {
-    streamThread.join();
-  }
+    if (streamThread.joinable())
+    {
+        streamThread.join();
+    }
 }
 
 void cvstream::read_source()
 {
-  do
-  {
-    // initialize a video capture object
-    cv::VideoCapture vid_capture(videosource);
-
-    // Print error message if the stream is invalid
-    if (!vid_capture.isOpened())
+    do
     {
-      cout << "Error opening video stream or file" << endl;
-    }
-    else
-    {
-      // Obtain fps and frame count by get() method and print
-      int fps = vid_capture.get(CAP_PROP_FPS);
-      cout << "Frames per second :" << fps;
-    }
+        // initialize a video capture object
+        cv::VideoCapture vid_capture(videosource);
 
-    streamer.start(8897);
-
-    // Read the frames to the last frame
-    while (vid_capture.isOpened())
-    {
-      // Initialise frame matrix
-      Mat frame;
-
-      // Initialize a boolean to check if frames are there or not
-      bool isSuccess = vid_capture.read(frame);
-
-      // If frames are present, show it
-      if (isSuccess == true)
-      {
-        std::lock_guard<std::mutex> guard(mutex);
-
-        bool flag = this->frame.empty();
-
-        this->frame = frame.clone();
-        if (flag) {
-          this->streamThread = std::thread(&cvstream::stream_publish, this);
+        // Print error message if the stream is invalid
+        if (!vid_capture.isOpened())
+        {
+            cout << "Error opening video stream or file" << endl;
         }
-      }
+        else
+        {
+            // Obtain fps and frame count by get() method and print
+            int fps = vid_capture.get(CAP_PROP_FPS);
+            cout << "Frames per second :" << fps;
+        }
 
-      if (isSuccess == false)
-      {
-        cout << "video end" << endl;
-        isWorking = false;
-        streamer.stop();
-        break;
-      }
+        streamer.start(8897);
 
-      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        // Read the frames to the last frame
+        while (vid_capture.isOpened())
+        {
+            // Initialise frame matrix
+            Mat frame;
 
-    }
-    // Release the video capture object
-    vid_capture.release();
+            // Initialize a boolean to check if frames are there or not
+            bool isSuccess = vid_capture.read(frame);
 
-    return;
+            // If frames are present, show it
+            if (isSuccess == true)
+            {
+                std::lock_guard<std::mutex> guard(mutex);
 
-  } while (false);
+                bool flag = this->frame.empty();
+
+                this->frame = frame.clone();
+                if (flag) {
+                    this->streamThread = std::thread(&cvstream::stream_publish, this);
+                }
+            }
+
+            if (isSuccess == false)
+            {
+                cout << "video end" << endl;
+                isWorking = false;
+                streamer.stop();
+                break;
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+        }
+        // Release the video capture object
+        vid_capture.release();
+
+        return;
+
+    } while (false);
 
 }
 
 void cvstream::stream_publish()
 {
-  std::vector<int> params = { cv::IMWRITE_JPEG_QUALITY, quality };
+    std::vector<int> params = { cv::IMWRITE_JPEG_QUALITY, quality };
 
-  while (isWorking)
-  {
-    cv::Mat frame;
+    while (isWorking)
     {
-      std::lock_guard<std::mutex> guard(mutex);
-      frame = this->frame.clone();
-      if (width > 0 && height > 0)
-      {
-        cv::resize(this->frame, frame, cv::Size(width, height));
-      }
+        cv::Mat frame;
+        {
+            std::lock_guard<std::mutex> guard(mutex);
+            if (width > 0 && height > 0)
+            {
+                cv::resize(this->frame, frame, cv::Size(width, height));
+            }
+            else
+            {
+                frame = this->frame.clone();
+            }
+
+            if (!frame.empty())
+            {
+                std::vector<uchar> buff_bgr;
+                cv::imencode(".jpg", frame, buff_bgr, params);
+                imgString = std::string(buff_bgr.begin(), buff_bgr.end());
+            }
+        }
+
+        streamer.publish("/" + stream_url, imgString);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
     }
-
-    if (!frame.empty())
-    {
-      std::vector<uchar> buff_bgr;
-      cv::imencode(".jpg", frame, buff_bgr, params);
-      streamer.publish("/" + stream_url, std::string(buff_bgr.begin(), buff_bgr.end()));
-    }
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-  }
 }
--- opencv_video_stream/cvstream.h
+++ opencv_video_stream/cvstream.h
@@ -6,38 +6,43 @@
 #include <atomic>
 #include <thread>
 #include <nadjieb/mjpeg_streamer.hpp>
-
+#include "3rdparty/cpp-httplib/httplib.h"
 #include <opencv2/opencv.hpp>
 
 class cvstream
 {
 private:
-  //rtsp url
-  std::string videosource;
+    //rtsp url
+    std::string videosource;
 
-  //http stream url
-  std::string stream_url;
+    //http stream url
+    std::string stream_url;
 
-  int height = 720;
-  int width = 1280;
-  int quality = 100;
+    int height = 720;
+    int width = 1280;
+    int quality = 100;
 
-  std::atomic_bool isWorking = true;
+    std::atomic_bool isWorking = true;
 
-  cv::Mat frame;
-  std::mutex mutex;
-  nadjieb::MJPEGStreamer streamer;
+    cv::Mat frame;
+    std::mutex mutex;
+    nadjieb::MJPEGStreamer streamer;
 
 private:
-  std::thread rtspThread;
-  std::thread streamThread;
+    std::thread rtspThread;
+    std::thread streamThread;
+
+    std::thread httpThread;
+
+    httplib::Server svr;
+    std::string imgString;
 
 public:
-  cvstream(std::string source, std::string dist, int width, int height, int quality);
-  ~cvstream();
-  void run();
+    cvstream(std::string source, std::string dist, int width, int height, int quality);
+    ~cvstream();
+    void run();
 
 private:
-  void read_source();
-  void stream_publish();
+    void read_source();
+    void stream_publish();
 };
